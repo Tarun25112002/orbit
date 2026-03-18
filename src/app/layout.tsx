a562d90136db10472814f402fabfe39e91bfc752
Tarun Kumar Jha <jhatarun14082002@gmail.com>
2026-03-19 01:48:14 +0530

    feat: integrate Clerk for authentication
    - Added @clerk/nextjs and @clerk/ui dependencies to package.json.
    - Updated layout.tsx to include ClerkProvider for authentication management.
    - Implemented sign-in and sign-up buttons, along with user button for signed-in users.
    - Created a new proxy.ts file to handle Clerk middleware for API routes and static files.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from "next";
+ import { dark } from "@clerk/ui/themes";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 
 import { Toaster } from "@/components/ui/sonner";
@@ -29,8 +37,27 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} ${plexMono.variable} antialiased`}>
-        {children}
-        <Toaster />
+        <ClerkProvider
+          appearance={{
+            theme: dark
+          }}
+        >
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+          {children}
+          <Toaster />
+        </ClerkProvider>
       </body>
     </html>
   );
